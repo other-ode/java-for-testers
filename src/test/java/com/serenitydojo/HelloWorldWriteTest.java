9039a42a6273cab678159b032edb8839752b58f1
jmx4real <odebade.digit@gmail.com>
2020-04-21 11:22:17 +0100

    Adding Class for Numerical Variables

--- src/test/java/com/serenitydojo/HelloWorldWriteTest.java
+++ src/test/java/com/serenitydojo/HelloWorldWriteTest.java
@@ -9,4 +9,20 @@ public class HelloWorldWriteTest {
         HelloWorldWriter writer = new HelloWorldWriter();
         writer.writeHelloWorld();
     }
+
+    @Test
+    public void declaringNumericalVariables(){
+        Integer age = 40;
+        age = age + 1;
+        System.out.println("Age is " + age.toString());
+
+        final int ageThiYear = 39;
+        int ageNextYear = ageThiYear + 1;
+        System.out.println("Current Age :" + ageThiYear);
+        System.out.println("Next Age :" + ageNextYear);
+        
+        final double pi = 3.141592;
+        long radius = 10;
+        System.out.println("The Area of a circle of radius " + radius + " = " + (pi*radius*radius));
+    }
 }
