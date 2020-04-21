b2fa9dfafc58bd63aaaf2c5f0449ecdbe57317c6
jmx4real <odebade.digit@gmail.com>
2020-04-21 12:24:43 +0100

    Marking variable as Private and creating getters

--- src/main/java/com/serenitydojo/Cat.java
+++ src/main/java/com/serenitydojo/Cat.java
@@ -2,7 +2,25 @@ package com.serenitydojo;
 
 public class Cat {
 
-    public String name;
-    public String favouriteFood;
-    public int age;
+    private String name;
+    private String favouriteFood;
+    private int age;
+
+     public Cat(String name, String favouriteFood, int age){
+        this.name = name;
+        this.favouriteFood = favouriteFood;
+        this.age = age;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getFavouriteFood(){
+        return favouriteFood;
+    }
+
+    public int getAge(){
+        return age;
+    }
 }
--- src/test/java/com/serenitydojo/WhenCreatingAnObject.java
+++ src/test/java/com/serenitydojo/WhenCreatingAnObject.java
@@ -10,17 +10,16 @@ public class WhenCreatingAnObject {
         String favouriteFood = "Tuna";
         int age = 3;
 
-        Cat felix = new Cat();
-        felix.name = "Felix";
-        felix.favouriteFood = "Tuna";
-        felix.age = 3;
+        Cat felix = new Cat("Felix","Tuna",3);
+//        felix.name = "Felix";
+//        felix.favouriteFood = "Tuna";
+//        felix.age = 3;
+        System.out.println("My " + felix.getAge() + " year old cat, named " + felix.getName() + " likes " + felix.getFavouriteFood());
 
-        System.out.println("My " + felix.age + " year old cat, named " + felix.name + " likes " + felix.favouriteFood);
-
-        Cat jumbo = new Cat();
-        jumbo.name = "Jumbo";
-        jumbo.age = 7;
-        System.out.println("My cat, " + jumbo.name + " is " + jumbo.age + "year old");
+        Cat jumbo = new Cat("Jumbo", "Salmon",7);
+//        jumbo.name = "Jumbo";
+//        jumbo.age = 7;
+        System.out.println("My cat, " + jumbo.getName() + " is " + jumbo.getAge() + "year old");
 
 
 
